25c22de084fd23ab711756bdfcadc16aeba28651
SHREESH-S <shreex24@gmail.com>
2026-09-11 21:09:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Change this before deploying online
-const API_KEY = "process.env.API_KEY";
+const API_KEY = process.env.API_KEY;
 
 app.use(express.json());
 
